29ec8c7378f809a6245743902a4472e552849642
Austin Auger <a.ustin.mark.auger@gmail.com>
2022-01-10 17:45:57 -0500

    Update main.rs
    RBE 1.2.2.1

--- rbe/src/main.rs
+++ rbe/src/main.rs
@@ -1,12 +1,31 @@
-use std::{vec, string, ops::Add};
+use std::fmt; // Import the `fmt` module.
 
-fn modify_string(input: String) -> String {
-    input.add("world!")
+// Define a structure named `List` containing a `Vec`.
+struct List(Vec<i32>);
+
+impl fmt::Display for List {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Extract the value using tuple indexing,
+        // and create a reference to `vec`.
+        let vec = &self.0;
+
+        write!(f, "[")?;
+
+        // Iterate over `v` in `vec` while enumerating the iteration
+        // count in `count`.
+        for (count, v) in vec.iter().enumerate() {
+            // For every element except the first, add a comma.
+            // Use the ? operator to return on errors.
+            if count != 0 { write!(f, ", ")?; }
+            write!(f, "{}: {}", count, v)?;
+        }
+
+        // Close the opened bracket and return a fmt::Result value.
+        write!(f, "]")
+    }
 }
 
 fn main() {
-    let vector = vec!["hello", "world"];
-    let mut result = vector.join(" ");
-    result = modify_string(result);
-    println!("{}", result);
+    let v = List(vec![1, 2, 3]);
+    println!("{}", v);
 }
